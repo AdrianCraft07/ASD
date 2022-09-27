acbe5a0d8930dab7d8dc9ef55c041b524a430fdd
AdrianCraft07 <proadrian684@gmail.com>
2022-09-26 23:53:03 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const IS_WEB = !!globalThis.window;
+globalThis._require = globalThis.require;
+
+if(!IS_WEB){
+  globalThis.fetch = globalThis.require('@agacraft/http').request;
+}
+
+if(IS_WEB){
+  globalThis.require = async function (path) {
+    const res = await fetch(path);
+    const text = await res.text();
+    const module = { exports: {} };
+    new Function('module', 'exports', text)(module, module.exports);
+    return module.exports;
+  };
+  globalThis.__dirname = globalThis.location.href.split('/').slice(0, -1).join('/');
+  globalThis.__filename = globalThis.location.href;
+}
+else{
+  globalThis.require = async function (path) {
+    return globalThis._require(path);
+  };
+  globalThis.__dirname = globalThis.process.cwd();
+  globalThis.__filename = globalThis.process.argv[1];
+}
