57fbcaf07841154a24e6947577ba711ff72a4c13
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-09-27 22:31:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
+"use strict";
 const IS_WEB = !!globalThis.window;
-
-if(!IS_WEB){
-  globalThis.fetch = globalThis.require('@agacraft/http').request;
-}
-
-if(IS_WEB){
-  globalThis.include = async function (path) {
-    const res = await fetch(path);
-    const text = await res.text();
-    const module = { exports: {} };
-    new Function('module', 'exports', 'require', text)(module, module.exports, globalThis.include);
-    return module.exports;
-  };
-  globalThis.__dirname = globalThis.location.href.split('/').slice(0, -1).join('/');
-  globalThis.__filename = globalThis.location.href;
+globalThis.require = require;
+const globalThisA = globalThis;
+if (!IS_WEB) {
+    const request = require('./request');
+    globalThisA.fetch = request;
 }
 else{
-  globalThis.include = async function (path) {
-    if(isURL(path)){
-      const res = await fetch(path);
-      const text = await res.text();
-      const module = { exports: {} };
-      new Function('module', 'exports', 'require', text)(module, module.exports, globalThis.include);
-      return module.exports;
-    }
-    return globalThis.require(path);
-  };
-  globalThis.__dirname = globalThis.process.cwd();
-  globalThis.__filename = globalThis.process.argv[1];
+    const module = {}
 }
 
-function isURL(path){
-  return path.startsWith('http://') || path.startsWith('https://')
+if (IS_WEB) {
+    globalThisA.include = async function (path) {
+        const res = await fetch(path);
+        const text = await res.text();
+        const module = { exports: {} };
+        new Function('module', 'exports', 'include', text)(module, module.exports, globalThisA.include);
+        return module.exports;
+    };
+    globalThisA.__dirname = globalThis.location.href.split('/').slice(0, -1).join('/');
+    globalThisA.__filename = globalThis.location.href;
 }
+else {
+    globalThisA.include = async function (path) {
+        if (isURL(path)) {
+            const res = await fetch(path);
+            const text = await res.text();
+            const module = { exports: {} };
+            new Function('module', 'exports', 'include', text)(module, module.exports, globalThisA.include);
+            return module.exports;
+        }
+        return globalThis.require(path);
+    };
+    globalThisA.__dirname = globalThis.process.cwd();
+    globalThisA.__filename = globalThis.process.argv[1];
+}
+function isURL(path) {
+    return path.startsWith('http://') || path.startsWith('https://');
+}
+
+module.exports = globalThisA.include;
